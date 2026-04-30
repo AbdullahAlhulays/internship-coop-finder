9a53ccfd9e635c0635ca1da3b20e1beed71735a0
AbdullahAlhulays <Abdullah.alhulays@gmail.com>
2026-05-01 01:07:54 +0300

    Add Bupa internship deadline

--- src/data/companies.js
+++ src/data/companies.js
@@ -431,6 +431,7 @@ export const companies = [
   name: "Bupa",
   location: "Jeddah, Saudi Arabia",
   applicationLink: "https://careers.bupa.com.sa/en/job-search-results/?keyword=coop&sort=22,d&page=1&view=grid",
+  deadline: "2026-05-13",
   type: "Internship",
   isNew: true,
 },
